2f4e2d171115d80a3074a3532b4447cc04313dbe
Jack Weaver <weaverjackop@gmail.com>
2022-07-11 11:59:58 -0300

    first message randomly printed.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 console.log('Whats up bitches!!!')
-const MessageOneComponent = ['red', 'blue', 'green', 'yellow'];
-const MessageTwoComponent = ['house','car','boat', 'tree'];
-const MessageThreeComponent = ['bananas','fish','cake','cheese'];
+const messageOneComponent = ['red', 'blue', 'green', 'yellow'];
+const messageTwoComponent = ['house','car','boat', 'tree'];
+const messageThreeComponent = ['bananas','fish','cake','cheese'];
+
+let randomCompOne = Math.floor(Math.random()*4);
+let randomCompTwo = Math.floor(Math.random()*4);
+let randomCompThree = Math.floor(Math.random()*4);
+
+console.log(`Your favourite color is ${messageOneComponent[randomCompOne]}`);
 
